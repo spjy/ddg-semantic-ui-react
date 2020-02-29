8969fbb451e9fde37ac20394ff0875b5f8c24b6e
spjy <spjy@hawaii.edu>
2020-02-29 12:55:02 -1000

    Task 3

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './style.css';
+import 'semantic-ui-css/semantic.min.css';
+import { Container, Header } from 'semantic-ui-react';
 
 class DuckDuckGo extends React.Component {
   render() {
     return (
-      <h1>DuckDuckGo!</h1>
+      <Container textAlign="center">
+        <h1>DuckDuckGo!</h1>
+      </Container>
     );
   }
 }
